ec4ddfe3f6e204a751e1f64f728ec7063ddb75e6
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-01-29 23:48:47 +0530

    Socail Media Link

--- src/pages/MediaLinks.jsx
+++ src/pages/MediaLinks.jsx
@@ -6,29 +6,74 @@ import Profile from "../components/Profile";
 import Skills from "../components/Skills";
 import Spotify from "../components/Spotify";
 import Twitter from "../components/Twitter";
-import '../style/Media.css';
 import Blog from "../components/Projects/Blog";
+import Hackerrank from "../components/Hackerrank";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, useEffect } from 'react';
+import '../style/Media.css';
 
 export const MediaLinks = () => {
 
+    const twitterRef = useRef();
+    const linkedInRef = useRef();
+    const githubRef = useRef();
+    const playerRef = useRef();
+    const instagramRef = useRef();
+    const hackerrankRef = useRef();
+
+
+    const animate = (element, direction, delay = 0, label) => ({
+        from: {
+            [direction.includes('x') ? 'x' : 'y']: direction.includes('-') ? '-100%' : '100%',
+            opacity: 0
+        },
+        to: {
+            [direction.includes('x') ? 'x' : 'y']: 0,
+            opacity: 1,
+            delay,
+            ease: "steps(1)"
+        },
+        label
+    });
+
+    useEffect(() => {
+        const tl = gsap.timeline();
+
+        // Main components
+        const mainAnimations = [
+            [twitterRef, '-x', 0.5],
+            [githubRef, 'x'],
+            [linkedInRef, '-x'],
+            [playerRef, '-x'],
+            [hackerrankRef, 'x'],
+            [instagramRef, '-x'],
+        ].forEach(([ref, dir, delay]) =>
+            tl.fromTo(ref.current, animate(ref, dir, delay).from, animate(ref, dir, delay).to)
+        );
+
+    }, []);
 
     return (
-        <div className="mediaLayout container mx-auto">
+        <div className="mediaLayout container mx-auto overflow-hidden">
 
-            <div className="box twitter h-full">
+            <div ref={twitterRef} className="box twitter h-full">
                 <Twitter />
             </div>
-            <div className="box linkedIn h-full">
+            <div ref={linkedInRef} className="box linkedIn h-full">
                 <LinkedIn />
             </div>
-            <div className="box github h-full">
+            <div ref={githubRef} className="box github h-full">
                 <Github />
             </div>
-            <div className="box spotify h-full">
+            <div ref={playerRef} className="box spotify h-full">
                 <Spotify />
             </div>
-            <div className="box insta h-full">
+            <div ref={instagramRef} className="box insta h-full">
                 <Instagram />
+            </div>
+            <div ref={hackerrankRef} className="box insta h-full">
+                <Hackerrank />
             </div>
 
             <div className="box box2 md:opacity-20 opacity-50 map h-full">
