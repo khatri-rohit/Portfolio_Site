f3e4d6936b7e93a032940e10c5b1c6a2613da9f0
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-01-29 23:48:26 +0530

    Project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,22 +7,61 @@ import Blog from "../components/Projects/Blog";
 import Skills from "../components/Skills";
 import Spotify from "../components/Spotify";
 import Twitter from "../components/Twitter";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, useEffect } from 'react';
 
 const Projects = () => {
-  
+
+  const projectRef = useRef();
+  const profileRef = useRef();
+  const githubRef = useRef();
+  const linkedInRef = useRef();
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const animate = (element, direction, delay = 0, label) => ({
+    from: {
+      [direction.includes('x') ? 'x' : 'y']: direction.includes('-') ? '-50%' : '50%',
+      opacity: 0
+    },
+    to: {
+      [direction.includes('x') ? 'x' : 'y']: 0,
+      opacity: 1,
+      delay,
+      ease: "steps(1)"
+    },
+    label
+  });
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    // Main components
+    const mainAnimations = [
+      [projectRef, '-x', 0.5],
+      [profileRef, 'y'],
+      [githubRef, 'x'],
+      [linkedInRef, 'x'],
+    ].forEach(([ref, dir, delay]) =>
+      tl.fromTo(ref.current, animate(ref, dir, delay).from, animate(ref, dir, delay).to)
+    );
+
+  }, []);
+
+
   return (
-    <div className="layout lg:w-[80%] w-full mx-auto lg:p-[4em] md:p-[1.5em] p-[1em]">
-      <div className="box probox blog h-full">
+    <div className="layout lg:w-[80%] w-full mx-auto lg:p-[4em] md:p-[1.5em] p-[1em] overflow-hidden">
+      <div ref={projectRef} className="box probox blog h-full">
         <Blog />
       </div>
-      <div className="box box1 profile h-full">
+      <div ref={profileRef} className="box box1 profile h-full">
         <Profile />
       </div>
-
-      <div className="box box3 w-full github h-full md:block hidden">
+      <div ref={githubRef} className="box box3 w-full github h-full md:block hidden">
         <Github />
       </div>
-      <div className="box box3 w-full linkedIn h-full md:block hidden">
+      <div ref={linkedInRef} className="box box3 w-full linkedIn h-full md:block hidden">
         <LinkedIn />
       </div>
 
